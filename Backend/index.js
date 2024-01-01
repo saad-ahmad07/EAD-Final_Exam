f30dbe84f56dd84f3ded60b6f8a7e150c78fbf7b
saad-ahmad07 <saadahmed.bscssef20@iba-suk.edu.pk>
2024-01-01 10:39:33 +0500

    Added two methods

--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,26 @@ import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
 import { connectDB } from "./config/database.js"
-
+import { Appointment } from "./model/TimeSlot.js"
+import { Slots } from "./model/TimeSlot.js"
+// Setup 
 const app = express()
-
 app.use(bodyParser.json())
 app.use(cors())
+connectDB()
 
-app.get("/",(req,res)=>{
-    res.json("Hello World Saad")
+
+// API end points 
+
+app.get("/home",(req,res)=>{ // Fetches all the time slots
+    Slots.find().then(slots => res.json(slots)).catch(err => res.json(err))
 })
+
+app.get("/home/app",(req,res)=>{ // Fetches all the appointments
+    Appointment.find().then(appoint => res.json(appoint)).catch(err => res.json(err))
+})
+
+
 
 app.listen(5000,()=>{
     console.log("Listening at 5000....");
--- Backend/model/TimeSlot.js
+++ Backend/model/TimeSlot.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-var Appointment = new mongoose.Schema({
+var slots = new mongoose.Schema({
+        startTime: String,
+        endTime: String
+})
+
+var app = new mongoose.Schema({
     startTime: String,
     endTime: String,
     user: Object
 })
 
-export const Appointment = mongoose.model('Appointment',Appointment)
+export const Appointment = mongoose.model('Appointments',app)
+export const Slots = mongoose.model('Slots',slots)
